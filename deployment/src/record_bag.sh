26afec0b16ce1506ba72d47fef5767915f5b9272
Ryujikoyasu <koyasuryu@icloud.com>
2024-10-11 12:03:18 +0900

    -

--- deployment/src/record_bag.sh
+++ deployment/src/record_bag.sh
@@ -1,29 +1,39 @@
 #!/bin/bash
 
+# Create a timestamp for the filename
+timestamp=$(date +"%Y_%m%d_%H%M")
+filename="rosbag_${timestamp}"
+
 # Create a new tmux session
 session_name="record_bag_$(date +%s)"
 tmux new-session -d -s $session_name
 
-# Split the window into three panes
+# Split the window into four panes
+tmux selectp -t 0    # select the first (0) pane
+tmux splitw -h -p 50 # split it into two halves
 tmux selectp -t 0    # select the first (0) pane
 tmux splitw -v -p 50 # split it into two halves
+tmux selectp -t 2    # select the new, second (2) pane
+tmux splitw -v -p 50 # split it into two halves
 tmux selectp -t 0    # go back to the first pane
-tmux splitw -h -p 50 # split it into two halves
 
-# Run the ros2 launch command in the first pane
+# robot setup
 tmux select-pane -t 0
-tmux send-keys "ros2 launch vint_locobot.launch.py" Enter
+tmux send-keys "ros2 launch om_modbus_master om_modbus_master_launch.py" Enter
+tmux send-keys "python3 /home/ryuddi/ROS2/om_modbus_master_V201/src/om_modbus_master/sample/BLV_R/twist_to_motor.py" Enter
 
-# Run the joy_teleop.py script in the second pane
+# camera and joycon setup
 tmux select-pane -t 1
-tmux send-keys "conda activate vint_deployment" Enter
+tmux send-keys "ros2 run gstreamer_camera gstreamer_camera_node" Enter
 tmux send-keys "ros2 launch teleop_twist_joy teleop-launch.py" Enter
-# tmux send-keys "python joy_teleop.py" Enter
 
-# Change the directory to ../topomaps/bags and run the ros2 bag record command in the third pane
+# twist_mux node setup
 tmux select-pane -t 2
-tmux send-keys "cd ../topomaps/bags" Enter
-tmux send-keys "ros2 bag record /image_raw" # change topic if necessary
+tmux send-keys "ros2 run twist_mux twist_mux --ros-args -p config_file:=/deployment/config/twist_mux.yaml" Enter
+
+# bag recording
+tmux select-pane -t 3
+tmux send-keys "cd ../topomaps/bags && ros2 bag record /image_raw -o ${filename}" Enter
 
 # Attach to the tmux session
 tmux -2 attach-session -t $session_name
--- deployment/src/vint_locobot.launch.py
+++ deployment/src/vint_locobot.launch.py
@@ -1,51 +1,13 @@
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, ExecuteProcess
 from launch_ros.actions import Node
-from launch.substitutions import LaunchConfiguration
 
 def generate_launch_description():
     return LaunchDescription([
-        # コマンドライン引数の宣言
-        DeclareLaunchArgument(
-            'config_file',
-            default_value='/ssd/source/navigation/visualnav-transformer-code_release/deployment/config/cmd_vel_mux.yaml',
-            description='Path to the velocity multiplexer config file'
-        ),
-
-        # 自作ロボットの立ち上げ
         Node(
-            package='om_modbus_master',
-            executable='om_modbus_master_node',
-            name='om_modbus_master',
-            output='screen'
-        ),
-        ExecuteProcess(
-            cmd=['python3', '/home/ryuddi/ROS2/om_modbus_master_V201/src/om_modbus_master/sample/BLV_R/twist_to_motor.py'],
-            output='screen'
-        ),
-
-        # カメラの立ち上げ
-        Node(
-            package='gstreamer_camera',
-            executable='gstreamer_camera_node',
-            name='gstreamer_camera',
-            output='screen'
-        ),
-
-        # Joyconの立ち上げ
-        Node(
-            package='teleop_twist_joy',
-            executable='teleop_node',
-            name='teleop_twist_joy_node',
-            output='screen'
-        ),
-
-        # Velocity Multiplexer の起動
-        Node(
-            package='yocs_cmd_vel_mux',
-            executable='cmd_vel_mux',
-            name='cmd_vel_mux',
-            parameters=[{'config_file': LaunchConfiguration('config_file')}],
+            package='twist_mux',
+            executable='twist_mux',
+            name='twist_mux',
+            parameters=['/deployment/config/twist_mux.yaml'],
             output='screen'
         ),
     ])
