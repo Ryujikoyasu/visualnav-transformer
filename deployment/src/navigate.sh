448fd198fdd415235a4967c7a1fcb8fa117f49d3
Ryujikoyasu <koyasuryu@icloud.com>
2024-10-12 02:22:15 +0900

    -

--- deployment/src/navigate.sh
+++ deployment/src/navigate.sh
@@ -16,17 +16,20 @@ tmux selectp -t 0    # go back to the first pane
 
 # Run the ros2 launch command in the first pane
 tmux select-pane -t 0
-tmux send-keys "ros2 launch vint_locobot.launch.py" Enter
+tmux send-keys "ros2 launch om_modbus_master om_modbus_master_launch.py" Enter
+tmux send-keys "python3 /home/ryuddi/ROS2/om_modbus_master_V201/src/om_modbus_master/sample/BLV_R/twist_to_motor.py" Enter
+tmux send-keys "ros2 run gstreamer_camera gstreamer_camera_node" Enter
+tmux send-keys "ros2 launch teleop_twist_joy teleop-launch.py joy_vel:=/cmd_vel_mux/input/teleop" Enter
 
 # Run the navigate.py script with command line args in the second pane
 tmux select-pane -t 1
-# tmux send-keys "conda activate vint_deployment" Enter
+tmux send-keys "conda activate vint_deployment" Enter
 tmux send-keys "python navigate.py $@" Enter
 
 # Run the joy_teleop.py script in the third pane
 tmux select-pane -t 2
 # tmux send-keys "conda activate vint_deployment" Enter
-tmux send-keys "python joy_teleop.py" Enter
+tmux send-keys "ros2 run twist_mux twist_mux --ros-args -p config_file:=/deployment/config/twist_mux.yaml" Enter
 
 # Run the pd_controller.py script in the fourth pane
 tmux select-pane -t 3
