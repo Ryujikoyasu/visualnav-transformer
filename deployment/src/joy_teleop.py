7d90116e3059cf8ca38141843ca0abaa8a44aa64
Ryujikoyasu <koyasuryu@icloud.com>
2024-10-31 19:37:23 +0900

    -

--- deployment/src/joy_teleop.py
+++ deployment/src/joy_teleop.py
@@ -31,6 +31,10 @@ class JoyTeleop(Node):
             self.get_logger().info(f'ジョイスティック名: {self.joystick.get_name()}')
             self.get_logger().info(f'軸の数: {self.joystick.get_numaxes()}')
             self.get_logger().info(f'ボタンの数: {self.joystick.get_numbuttons()}')
+            
+            # 全ての軸の初期値を表示
+            for i in range(self.joystick.get_numaxes()):
+                self.get_logger().info(f'軸 {i} の初期値: {self.joystick.get_axis(i)}')
         except pygame.error:
             self.get_logger().error('ジョイスティックが見つかりません')
             sys.exit(1)
@@ -39,18 +43,11 @@ class JoyTeleop(Node):
         self.linear_speed = 0.5  # 直進速度の最大値
         self.angular_speed = 1.0  # 回転速度の最大値
         
-        # Joy-Con (L)の軸の設定
-        self.STICK_HORIZONTAL = 0  # 左スティックの水平方向
-        self.STICK_VERTICAL = 1    # 左スティックの垂直方向
-        
         # デッドゾーンの設定
         self.deadzone = 0.15
         
         # タイマーの設定（20Hz）
         self.create_timer(0.05, self.timer_callback)
-        
-        # 前回の軸の値を保存
-        self.prev_axes = [0.0] * self.joystick.get_numaxes()
 
     def apply_deadzone(self, value, deadzone):
         if abs(value) < deadzone:
@@ -59,26 +56,42 @@ class JoyTeleop(Node):
 
     def timer_callback(self):
         # イベントの処理
-        pygame.event.pump()
+        for event in pygame.event.get():
+            # 全てのイベントタイプを出力
+            self.get_logger().info(f'イベントタイプ: {event.type}')
+            
+            if event.type == pygame.JOYAXISMOTION:
+                self.get_logger().info(f'軸の動き - 軸: {event.axis}, 値: {event.value:.3f}')
+            elif event.type == pygame.JOYBUTTONDOWN:
+                self.get_logger().info(f'ボタン押下 - ボタン: {event.button}')
+            elif event.type == pygame.JOYBUTTONUP:
+                self.get_logger().info(f'ボタン解放 - ボタン: {event.button}')
+            elif event.type == pygame.JOYHATMOTION:
+                self.get_logger().info(f'HAT変化 - HAT: {event.hat}, 値: {event.value}')
+            elif event.type == pygame.JOYDEVICEADDED:
+                self.get_logger().info('ジョイスティックが接続されました')
+            elif event.type == pygame.JOYDEVICEREMOVED:
+                self.get_logger().info('ジョイスティックが切断されました')
         
-        # 全ての軸の値をチェック
-        for i in range(self.joystick.get_numaxes()):
-            current_value = self.joystick.get_axis(i)
-            # 値が変化した場合のみ出力
-            if abs(current_value - self.prev_axes[i]) > 0.01:
-                self.get_logger().info(f'軸 {i} の値: {current_value:.3f}')
-                self.prev_axes[i] = current_value
+        # 現在の全ての軸の値を取得
+        axes_values = [self.joystick.get_axis(i) for i in range(self.joystick.get_numaxes())]
+        # 値が0でない軸のみ表示
+        for i, value in enumerate(axes_values):
+            if abs(value) > self.deadzone:
+                self.get_logger().info(f'軸 {i} の現在値: {value:.3f}')
         
         # Twistメッセージの作成
         msg = Twist()
         
-        # スティックの値を取得
-        horizontal = self.apply_deadzone(self.joystick.get_axis(self.STICK_HORIZONTAL), self.deadzone)
-        vertical = self.apply_deadzone(self.joystick.get_axis(self.STICK_VERTICAL), self.deadzone)
-        
-        # 速度指令値の計算
-        msg.linear.x = -vertical * self.linear_speed
-        msg.angular.z = -horizontal * self.angular_speed
+        # 全ての軸を試してみる
+        for i in range(self.joystick.get_numaxes()):
+            value = self.apply_deadzone(self.joystick.get_axis(i), self.deadzone)
+            if abs(value) > self.deadzone:
+                # 各軸の値を試験的に使用
+                if i in [0, 2]:  # 水平方向の可能性がある軸
+                    msg.angular.z = -value * self.angular_speed
+                elif i in [1, 3]:  # 垂直方向の可能性がある軸
+                    msg.linear.x = -value * self.linear_speed
         
         # 速度指令値をパブリッシュ
         self.publisher.publish(msg)
