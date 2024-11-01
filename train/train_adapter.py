bbc96c7e044a1505d0a169340370ff8a18dd48a3
Ryujikoyasu <koyasuryu@icloud.com>
2024-11-01 12:51:45 +0900

    ー

--- train/train_adapter.py
+++ train/train_adapter.py
@@ -23,8 +23,11 @@ def main(config):
     
     # データセットの準備
     transform = transforms.Compose([
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], 
-                             std=[0.229, 0.224, 0.225]),
+        transforms.ToTensor(),
+        transforms.Normalize(
+            mean=[0.485, 0.456, 0.406],
+            std=[0.229, 0.224, 0.225]
+        )
     ])
     
     # データローダーの作成
